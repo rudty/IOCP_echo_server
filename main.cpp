51a56213f76a6b4a70314787ae1c44c7a3d6e0b2
rudty <rudtyz@gmail.com>
2019-10-27 10:16:31 +0900

    callback으로 변경

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <string>
-#include <iostream>
 #include "socket.h"
 #pragma comment(lib, "ws2_32.lib")
 
@@ -11,26 +9,38 @@
 // 웹브라우저
 // 127.0.0.1:8087
 
+void onDataReceived(ClientSocket* c, const char* buf, int len) {
+	printf("%.*s\n", len, buf);
+	socket_tcp_client_send(c, buf, len);
+}
+
+void onConnectionClosed(ClientSocket* c) {
+	printf("disconnected\n");
+}
+
+void onSendComplection(ClientSocket* c) {
+	printf("send fin\n");
+}
+
+void onClientAccept(ClientSocket* c) {
+	printf("new client\n");
+
+	//전송 완료 시
+	socket_tcp_client_socket_set_send_complection_handler(c, onSendComplection);
+
+	//연결 종료 시
+	socket_tcp_client_socket_set_connection_close_handler(c, onConnectionClosed);
+
+	//데이터 입력 시
+	socket_tcp_client_socket_set_data_receive_handler(c, onDataReceived);
+}
+
 int main() {
 	ServerSocket* s;
 	socket_tcp_server_socket_initialize(&s);
-	socket_tcp_server_socket_set_accept_handler(s, [](ClientSocket* c) {
-		printf("new client\n");
-		
-		socket_tcp_client_socket_set_send_complection_handler(c, [](ClientSocket* c) {
-			printf("send finish\n");
-		});
 
-		socket_tcp_client_socket_set_connection_close_handler(c, [](ClientSocket* c) {
-			printf("disconnected\n");
-		});
-
-		socket_tcp_client_socket_set_data_receive_handler(c, [](ClientSocket* c, const char* buf, int len) {
-			std::string str(buf, len);
-			std::cout << str << std::endl;
-			socket_tcp_client_send(c, buf, len);
-		});
-	});
+	//클라이언트 입력시 호출할 함수
+	socket_tcp_server_socket_set_accept_handler(s, onClientAccept);
 
 	if (socket_tcp_server_socket_listen(s, 8087)) {
 		printf("8087 is already use");
